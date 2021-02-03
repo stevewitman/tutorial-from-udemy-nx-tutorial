4fba92f478cd4f8f06c9e77a4cb3dd48f9cd9adb
Steve Witman <stevewitman@gmail.com>
2021-02-03 10:46:52 -0700

    add ngrx to lib: client-auth

--- libs/client/auth/src/lib/client-auth.module.ts
+++ libs/client/auth/src/lib/client-auth.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SigninComponent } from './containers/signin/signin.component';
 import { SigninFormComponent } from './components/signin-form/signin-form.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromAuth from './+state/auth.reducer';
+import { AuthEffects } from './+state/auth.effects';
 
 @NgModule({
   imports: [
@@ -11,9 +15,11 @@ import { SigninFormComponent } from './components/signin-form/signin-form.compon
       {
         path: '',
         pathMatch: 'full',
-        component: SigninComponent 
-      }
+        component: SigninComponent,
+      },
     ]),
+    StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects]),
   ],
   declarations: [SigninComponent, SigninFormComponent],
 })
